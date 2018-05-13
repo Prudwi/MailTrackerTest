6a3829dfaa45d031032bb285a80a33b69c9a9dca
prudw <prudwichowdary@gmail.com>
2018-05-13 17:26:48 -0400

    updated step definition

--- MailTrackerAutomation/src/test/java/stepDefinitions/CustomerFeatureStepDefinition.java
+++ MailTrackerAutomation/src/test/java/stepDefinitions/CustomerFeatureStepDefinition.java
@@ -1,10 +1,15 @@
 package stepDefinitions;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Feature")
 public class CustomerFeatureStepDefinition {
 	@Given("^I want to write a step with precondition$")
 	public void i_want_to_write_a_step_with_precondition() throws Throwable {
