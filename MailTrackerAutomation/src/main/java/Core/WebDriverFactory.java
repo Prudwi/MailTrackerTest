8bcb8ca4d5906300726f8faaa6f4e5be91c9449a
prudw <prudwichowdary@gmail.com>
2018-05-13 17:19:19 -0400

    Updated webdriver factory

--- MailTrackerAutomation/src/main/java/Core/WebDriverFactory.java
+++ MailTrackerAutomation/src/main/java/Core/WebDriverFactory.java
@@ -1,11 +1,18 @@
 package Core;
 
+import java.util.Dictionary;
+
 import org.openqa.selenium.WebDriver;
 
 public class WebDriverFactory {
+	public Dictionary<String, String> WebDriverDict = null;
 public void GetCurrentWebDriver()
 {
 	
 	this.GetCurrentWebDriver();
+}
+public void instantiateDriver()
+{
+	//Instantiate webdriver
 }
 }
