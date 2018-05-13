5c335dcee860cc53396c7c7c4677c0cdc637f62c
prudw <prudwichowdary@gmail.com>
2018-05-13 17:42:09 -0400

    updated customer feature

--- MailTrackerAutomation/src/test/java/features/MailTrackerCustomerFeatures.feature
+++ MailTrackerAutomation/src/test/java/features/MailTrackerCustomerFeatures.feature
@@ -1,9 +1,21 @@
-@tag
-Feature: Title of your feature
-  I want to use this template for my feature file
+@CustomerAdminScenarios
+Feature: Customer Administration Test Scenarios
 
-  @tag1
-  Scenario: Title of your scenario
+  @AddNewCustomer
+  Scenario: Add a new customer to a financial institution
+  Given user is on customer administration page
+  When user click on Add New Button
+  Then validate user is on AddOrEditCustomer page
+  When user input Jim Johnson in Name TextBox
+  And user input a number in account number TextBox
+  When user click on save Button
+  Then validate success is displayed
+  And new customer is displayed in Customer Administration grid
+  @EditExistingCustomer
+  Scenario: Edit an existing customer
+  
+  
+  
    
 
   
--- MailTrackerAutomation/src/test/java/stepDefinitions/CustomerFeatureStepDefinition.java
+++ MailTrackerAutomation/src/test/java/stepDefinitions/CustomerFeatureStepDefinition.java
@@ -11,11 +11,52 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "Feature")
 public class CustomerFeatureStepDefinition {
-	@Given("^I want to write a step with precondition$")
-	public void i_want_to_write_a_step_with_precondition() throws Throwable {
+	@Given("^user is on customer administration page$")
+	public void user_is_on_customer_administration_page() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
 	}
 
+	@When("^user click on Add New Button$")
+	public void user_click_on_Add_New_Button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^validate user is on AddOrEditCustomer page$")
+	public void validate_user_is_on_AddOrEditCustomer_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user input Jim Johnson in Name TextBox$")
+	public void user_input_Jim_Johnson_in_Name_TextBox() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user input a number in account number TextBox$")
+	public void user_input_a_number_in_account_number_TextBox() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user click on save Button$")
+	public void user_click_on_save_Button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^validate success is displayed$")
+	public void validate_success_is_displayed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^new customer is displayed in Customer Administration grid$")
+	public void new_customer_is_displayed_in_Customer_Administration_grid() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 	
 }
