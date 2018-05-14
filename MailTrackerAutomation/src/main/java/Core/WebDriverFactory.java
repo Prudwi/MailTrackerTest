47ec0079cc24d5f502d4412b63d770cca56619ec
prudw <prudwichowdary@gmail.com>
2018-05-13 21:27:55 -0400

    updated webdriver factory

--- MailTrackerAutomation/src/main/java/Core/WebDriverFactory.java
+++ MailTrackerAutomation/src/main/java/Core/WebDriverFactory.java
@@ -2,17 +2,50 @@ package Core;
 
 import java.util.Dictionary;
 
+import org.junit.internal.builders.JUnit4Builder;
+import org.junit.runners.JUnit4;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+
+import cucumber.runtime.junit.JUnitReporter;
 
 public class WebDriverFactory {
-	public Dictionary<String, String> WebDriverDict = null;
+	public static Dictionary<String, WebDriver> WebDriverDict = null;
+	private static Object DictLock = new Object();
+	private static Boolean pageTrackerInit = false;
+	public static String browser = null;
+	
 public void GetCurrentWebDriver()
 {
 	
 	this.GetCurrentWebDriver();
 }
-public void instantiateDriver()
+public static String GetTestFixtureName()
 {
-	//Instantiate webdriver
+	String fixtureName = junit.runner.BaseTestRunner.SUITE_METHODNAME.toString();
+	
+	return fixtureName;
+}
+public static void InstantiateWebDriver()
+{
+	WebDriver driver;
+	String fixtureName = GetTestFixtureName();
+	if( browser == "IE")
+	{
+		InternetExplorerOptions IeOptions = new InternetExplorerOptions();
+		IeOptions.ignoreZoomSettings();		
+		driver = new InternetExplorerDriver(IeOptions);
+		driver.manage().window().maximize();
+	}
+	else 
+	{
+		ChromeOptions chrOptions = new ChromeOptions();
+		chrOptions.addArguments("start-maximized");
+		driver = new ChromeDriver(chrOptions);
+	}
+	WebDriverDict.put(fixtureName, driver);
 }
 }
